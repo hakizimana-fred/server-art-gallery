60cd0a4091c522d818e49cf2064dc3fb3fdbfb25
hakifred <hakifred20@gmail.com>
2023-02-15 00:21:18 +0300

    set up linters

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,23 @@
-console.log('server starting')
+import 'dotenv/config'
+import express from 'express'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+const app = express()
+
+const main = async () => {
+  try {
+    // middlewares
+    app.use(express.json())
+    app.use(helmet())
+    process.env.NODE_ENV === 'development' && app.use(morgan('dev'))
+
+    app.listen(process.env.PORT, () => {
+      console.log(`server started on http://localhost:${process.env.PORT}`)
+    })
+  } catch (err) {
+    process.exit(1)
+  }
+}
+
+main().catch((err) => console.log(err))
