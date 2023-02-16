f0179466bf624fd1d746ab37a1a80048662ac237
hakifred <hakifred20@gmail.com>
2023-02-17 01:20:57 +0300

    Update database.spec.ts

--- test/database.spec.ts
+++ test/database.spec.ts
@@ -1,5 +1,19 @@
-describe('Simple test case', () => {
-  it('Add two numbers', () => {
-    expect(2 + 2).toBe(4)
+import 'dotenv/config'
+import { connect, connection, set } from 'mongoose'
+set('strictQuery', true)
+// database connection tests
+describe('Database connection', () => {
+  beforeAll(async () => {
+    await connect('mongodb://localhost/artgallery')
+    await connection.db.dropDatabase()
+  })
+
+  it('Must connect to mongodb', async () => {
+    const db = await connect('mongodb://localhost/artgallery')
+    expect(db).toBeDefined()
+  })
+
+  afterAll(async () => {
+    await connection.close()
   })
 })
